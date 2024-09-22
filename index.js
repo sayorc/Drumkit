61b1258b6207b86fa40ba4d8289dac82d4f75cba
sayorc <63996902+sayorc@users.noreply.github.com>
2024-09-22 13:56:59 +0530

    Js Updated

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
-  document.querySelectorAll("button")[i].addEventListener("click", function () {
-    switch (this.textContent) {
-      case "w":
-        var audio = new Audio('./sounds/tom-1.mp3');
-        audio.play();
-        break;
-      case "a":
-        var audio = new Audio('./sounds/tom-2.mp3');
-        audio.play();
-        break;
-      case "s":
-        var audio = new Audio('./sounds/tom-3.mp3');
-        audio.play();
-        break;
-      case "d":
-        var audio = new Audio('./sounds/tom-4.mp3');
-        audio.play();
-        break;
-      case "j":
-        var audio = new Audio('./sounds/crash.mp3');
-        audio.play();
-        break;
-      case "k":
-        var audio = new Audio('./sounds/kick-bass.mp3');
-        audio.play();
-        break;
-      case "l":
-        var audio = new Audio('./sounds/snare.mp3');
-        audio.play();
-        break;
-    };
-  });
-};
+    document.querySelectorAll("button")[i].addEventListener("click", function () {
+      keypress1(this.textContent);
+    });
+  };
+
+  document.addEventListener("keydown", function(event){
+    keypress1(event.key);
+  })
+
+  function keypress1(key){
+    switch (key) {
+        case "w":
+          var audio = new Audio('./sounds/tom-1.mp3');
+          audio.play();
+          break;
+        case "a":
+          var audio = new Audio('./sounds/tom-2.mp3');
+          audio.play();
+          break;
+        case "s":
+          var audio = new Audio('./sounds/tom-3.mp3');
+          audio.play();
+          break;
+        case "d":
+          var audio = new Audio('./sounds/tom-4.mp3');
+          audio.play();
+          break;
+        case "j":
+          var audio = new Audio('./sounds/crash.mp3');
+          audio.play();
+          break;
+        case "k":
+          var audio = new Audio('./sounds/kick-bass.mp3');
+          audio.play();
+          break;
+        case "l":
+          var audio = new Audio('./sounds/snare.mp3');
+          audio.play();
+          break;
+      };
+  }
